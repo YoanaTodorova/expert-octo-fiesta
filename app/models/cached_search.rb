5cdc98b9c96fcf07fa5a6d16509702d19447fc23
YoanaTodorova <{ID}+{username}@users.noreply.github.com>
2024-04-26 11:20:18 +0200

    Add recent scope to CachedSearch model

--- app/models/cached_search.rb
+++ app/models/cached_search.rb
@@ -1,4 +1,10 @@
 class CachedSearch < ApplicationRecord
   has_many :cached_movie_searches, inverse_of: :cached_search, dependent: :destroy
   has_many :movies, through: :cached_movie_searches
+
+  CACHE_TTL = 2.minutes
+
+  def self.recent
+    where("created_at >= ?", Time.now - CACHE_TTL)
+  end
 end
