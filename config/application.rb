e0cee24b19a197b1e62dd1a4db118d4c2e65e7f0
YoanaTodorova <{ID}+{username}@users.noreply.github.com>
2024-04-27 10:03:31 +0200

    Setup sidekiq

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -34,9 +34,12 @@ module ExpertOctoFiesta
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << "#{config.root}/jobs"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.active_job.queue_adapter = :sidekiq
+
   end
 end
